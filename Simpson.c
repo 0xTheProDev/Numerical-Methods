2363c465dfcfbc8c6f3a88c58b32ca7f9471943d
Pragyan Bhattacharya <bprogyan@gmail.com>
2017-04-06 18:34:03 +0530

    Previously Added Files

---
+++ Simpson.c
@@ -0,0 +1,39 @@
+/*
+ * IMPLEMENTATION OF SIMPSON 1/3RD RULE
+ * FOR CALCULATING AREA UNDER CURVE
+ *
+ * f : USER GIVEN FUNCTION
+ * a : LOWER LIMIT OF INTIGRATION
+ * b : UPPER LIMIT OF INTIGRATION
+ * h : FIXED DIFFERENCE IN INTERVAL (LIMIT -> 0)
+ * n : FIXED NUMBER OF INTERVAL (LIMIT --> infinity)
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159265
+
+typedef double F(double);
+
+double simpson(double, double, F);
+
+double func(double a){
+	return sqrt(sin(a));
+}
+
+int main(void){
+	double a = 0, b = PI / 2;
+	double area = simpson(a, b, func);
+	printf("Area: %g\n", area);
+	return 0;
+}
+
+double simpson(const double a, const double b, F f){
+	const int n = 100;
+	const double h = (b - a) / n;
+	double sum = f(a) + f(b);
+	for(int i = 1; i < n; i++)
+		sum += i & 1 ? 4 * f(a + i * h) : 2 * f(a + i * h);
+	return (sum * h / 3);
+}
---
+++ Trapezoidal.c
@@ -0,0 +1,47 @@
+/*
+ * IMPLEMENTATION OF TRAPEZOIDAL RULE
+ * FOR CALCULATING AREA UNDER CURVE
+ *
+ * f : USER GIVEN FUNCTION
+ * a : LOWER LIMIT OF INTIGRATION
+ * b : UPPER LIMIT OF INTIGRATION
+ * h : FIXED DIFFERENCE IN INTERVAL (LIMIT -> 0)
+ * n : FIXED NUMBER OF INTERVAL (LIMIT --> infinity)
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159265359
+
+// Function calculating Area by Trapezoidal Rule
+static double trap(double f(double), const double a, const double b)
+{
+	if(a >= b)
+		return 0.0;
+	const double h = 0.01;
+	const unsigned long long n = ceil((b - a) / h);
+	double area = f(a) + f(b), t = a;
+	unsigned long long i = 1;
+	while(i < n){
+			t += h;
+			if(t >= b)
+					break;
+			area += 2 * f(t);
+	}
+	return 0.5 * area * h;
+}
+
+// User-defined Function
+double F(double x)
+{
+	return (sqrt(sin(x)));
+}
+
+int main(void) {
+	double a = 0, b = PI / 2;
+	double I = trap(F, a, b);
+	printf("Area: %g\n", I);
+	return 0;
+}
+
